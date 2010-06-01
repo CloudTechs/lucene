422156c8c574fe8efc67bce156df224583254ad5
Michael McCandless <mikemccand@apache.org>
2010-06-01 18:35:25 +0000

    LUCENE-2104: in NativeFSLockFactory, don't silently fail to release if lock is held by another process
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/branches/lucene_2_9@950196 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/store/NativeFSLockFactory.java
+++ src/java/org/apache/lucene/store/NativeFSLockFactory.java
@@ -144,7 +144,7 @@ public class NativeFSLockFactory extends FSLockFactory {
       }
     }
   }
-};
+}
 
 class NativeFSLock extends Lock {
 
@@ -296,6 +296,24 @@ class NativeFSLock extends Lock {
       }
       if (!path.delete())
         throw new LockReleaseFailedException("failed to delete " + path);
+    } else {
+      // if we don't hold the lock, and somebody still called release(), for
+      // example as a result of calling IndexWriter.unlock(), we should attempt
+      // to obtain the lock and release it. If the obtain fails, it means the
+      // lock cannot be released, and we should throw a proper exception rather
+      // than silently failing/not doing anything.
+      boolean obtained = false;
+      try {
+        if (!(obtained = obtain())) {
+          throw new LockReleaseFailedException(
+              "Cannot forcefully unlock a NativeFSLock which is held by another indexer component: "
+                  + path);
+        }
+      } finally {
+        if (obtained) {
+          release();
+        }
+      }
     }
   }
 
