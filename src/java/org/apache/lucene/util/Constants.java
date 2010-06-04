531e1ab8fb00e46dac6294523e25b618eab88a17
Uwe Schindler <uschindler@apache.org>
2010-06-04 12:17:39 +0000

    Prepare the 2.9.3 release
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/branches/lucene_2_9@951387 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lucene/util/Constants.java
+++ src/java/org/apache/lucene/util/Constants.java
@@ -73,7 +73,7 @@ public final class Constants {
     return s.toString();
   }
    
-  public static final String LUCENE_MAIN_VERSION = ident("2.9.2");
+  public static final String LUCENE_MAIN_VERSION = ident("2.9.3");
 
   public static final String LUCENE_VERSION;
   static {
