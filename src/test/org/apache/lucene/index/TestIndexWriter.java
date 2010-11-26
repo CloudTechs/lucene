17e8c71b73811a3eb124816189cd608fd5afac16
Michael McCandless <mikemccand@apache.org>
2010-11-26 11:50:23 +0000

    LUCENE-2776: don't create term vectors files if no doc uses term vectors
    git-svn-id: https://svn.apache.org/repos/asf/lucene/java/branches/lucene_2_9@1039322 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/lucene/index/TestIndexWriter.java
+++ src/test/org/apache/lucene/index/TestIndexWriter.java
@@ -4907,4 +4907,39 @@ public class TestIndexWriter extends BaseTokenStreamTestCase {
 
     dir.close();
   }
+
+  public void testNoUnwantedTVFiles() throws Exception {
+
+    Directory dir = new MockRAMDirectory();
+    IndexWriter indexWriter = new IndexWriter(dir, new WhitespaceAnalyzer(), IndexWriter.MaxFieldLength.UNLIMITED);
+    indexWriter.setRAMBufferSizeMB(0.01);
+    indexWriter.setUseCompoundFile(false);
+
+    String BIG="alskjhlaksjghlaksjfhalksvjepgjioefgjnsdfjgefgjhelkgjhqewlrkhgwlekgrhwelkgjhwelkgrhwlkejg";
+    BIG=BIG+BIG+BIG+BIG;
+
+    for (int i=0; i<2; i++) {
+      Document doc = new Document();
+      doc.add(new Field("id", Integer.toString(i)+BIG, Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS));
+      doc.add(new Field("str", Integer.toString(i)+BIG, Field.Store.YES, Field.Index.NOT_ANALYZED));
+      doc.add(new Field("str2", Integer.toString(i)+BIG, Field.Store.YES, Field.Index.ANALYZED));
+      doc.add(new Field("str3", Integer.toString(i)+BIG, Field.Store.YES, Field.Index.ANALYZED_NO_NORMS));
+      indexWriter.addDocument(doc);
+    }
+
+    indexWriter.close();
+
+    _TestUtil.checkIndex(dir);
+
+    assertNoUnreferencedFiles(dir, "no tv files");
+    String[] files = dir.listAll();
+    for(int idx=0;idx<files.length;idx++) {
+      String file = files[idx];
+      assertTrue(!file.endsWith(IndexFileNames.VECTORS_FIELDS_EXTENSION));
+      assertTrue(!file.endsWith(IndexFileNames.VECTORS_INDEX_EXTENSION));
+      assertTrue(!file.endsWith(IndexFileNames.VECTORS_DOCUMENTS_EXTENSION));
+    }
+
+    dir.close();
+  }
 }
